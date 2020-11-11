fdb625d253d9c5df6b8e4b074acb8fa6ebf73942
Kyrylo Antoshyn <akirill98@gmail.com>
2020-11-11 17:03:59 +0200

    Refactored script logic, added comments.

--- coco-to-yolo-by-category.py
+++ coco-to-yolo-by-category.py
@@ -1,69 +1,160 @@
-from pycocotools.coco import COCO
+import os
+import sys
+import shutil
 import requests
+import math
+from pycocotools.coco import COCO
+
+DATASET_DIRECTORY_NAME = "dataset"
+NUMBER_OF_DECIMALS_TO_LEAVE_IN_ANNOTATIONS = 7
 
 
-# Truncates numbers to N decimals
-def truncate(n, decimals=0):
-    multiplier = 10 ** decimals
-    return int(n * multiplier) / multiplier
+def initialize_coco():
+    """
+    Initializes COCO object.
+
+    :return: whether COCO object has been initialized and its reference
+    """
+    annotations_file_path = input("Enter path to the annotations file: ")
+    if not os.path.exists(annotations_file_path):
+        return False, None
+
+    return True, COCO(annotations_file_path)
 
 
-# Download instances_train2017.json from the COCO website and put in the same directory as this script
-coco = COCO('instances_train2017.json')
-cats = coco.loadCats(coco.getCatIds())
-nms=[cat['name'] for cat in cats]
-print('COCO categories: \n{}\n'.format(' '.join(nms)))
+def get_category_id_and_images(coco):
+    """
+    Gets category ID and images for the particular category.
+
+    :param coco: COCO object
+    :return: whether category exists, category ID and images for this category
+    """
+    category_names = [category["name"] for category in coco.loadCats(coco.getCatIds())]
+    print("COCO categories:", " ".join(category_names))
+
+    category_name_to_download = input("Enter category name to download: ")
+    if category_name_to_download not in category_names:
+        return False, None, None
+
+    category_id = coco.getCatIds(catNms=category_name_to_download)[0]
+
+    return True, category_id, coco.loadImgs(coco.getImgIds(catIds=category_id))
 
 
-# Replace category with whatever is of interest to you
-cat = "person"
-catIds = coco.getCatIds(catNms=[cat])
-imgIds = coco.getImgIds(catIds=catIds )
-images = coco.loadImgs(imgIds)
-# print("imgIds: ", imgIds)
-# print("images: ", images)
+def filter_images_to_download(images):
+    """
+    Filters images to download.
+
+    :param images: images
+    :return: whether images to download have been filtered and list of these images
+    """
+    images_total_number = len(images)
+    print("Total number of images:", images_total_number)
+
+    try:
+        number_of_images_to_download = int(input("Enter number of images to download: "))
+        if number_of_images_to_download <= 0 or number_of_images_to_download > images_total_number:
+            return False, None
+
+        return True, images[:number_of_images_to_download]
+    except ValueError:
+        return False, None
 
 
-# Create a subfolder in this directory called "downloaded_images". This is where your images will be downloaded into.
-# Comment this entire section out if you don't want to download the images
-for im in images:
-    print("im: ", im['file_name'])
-    img_data = requests.get(im['coco_url']).content
-    with open('downloaded_images/' + im['file_name'], 'wb') as handler:
-        handler.write(img_data)
-
-# Create a subfolder in this directory called "labels". This is where the annotations will be saved in YOLO format
-for im in images:
-    dw = 1. / im['width']
-    dh = 1. / im['height']
+def download_and_save_images(images_to_download):
+    """
+    Downloads images to the dataset directory.
     
-    annIds = coco.getAnnIds(imgIds=im['id'], catIds=catIds, iscrowd=None)
-    anns = coco.loadAnns(annIds)
-    
-    filename = im['file_name'].replace(".jpg", ".txt")
-    print(filename)
+    :param images_to_download: images to download
+    """
+    if os.path.exists(DATASET_DIRECTORY_NAME):
+        shutil.rmtree(DATASET_DIRECTORY_NAME)
+    os.makedirs(DATASET_DIRECTORY_NAME)
 
-    with open("labels/" + filename, "a") as myfile:
-        for i in range(len(anns)):
-            xmin = anns[i]["bbox"][0]
-            ymin = anns[i]["bbox"][1]
-            xmax = anns[i]["bbox"][2] + anns[i]["bbox"][0]
-            ymax = anns[i]["bbox"][3] + anns[i]["bbox"][1]
-            
-            x = (xmin + xmax)/2
-            y = (ymin + ymax)/2
-            
-            w = xmax - xmin
-            h = ymax-ymin
-            
-            x = x * dw
-            w = w * dw
-            y = y * dh
-            h = h * dh
-            
-            # Note: This assumes a single-category dataset, and thus the "0" at the beginning of each line.
-            mystring = str("0 " + str(truncate(x, 7)) + " " + str(truncate(y, 7)) + " " + str(truncate(w, 7)) + " " + str(truncate(h, 7)))
-            myfile.write(mystring)
-            myfile.write("\n")
+    for image_to_download in images_to_download:
+        print("Downloading and writing:", image_to_download["file_name"])
+        with open(os.path.join(DATASET_DIRECTORY_NAME, image_to_download["file_name"]), "wb") as image_file:
+            image_file.write(requests.get(image_to_download["coco_url"]).content)
 
-    myfile.close()
+
+def convert_and_write_annotations(coco, downloaded_images, category_id):
+    """
+    Converts and writes annotations.
+
+    :param coco: COCO object
+    :param downloaded_images: downloaded images
+    :param category_id: category ID
+    """
+
+    def truncate(number, number_of_decimals=0):
+        """
+        Truncates number by specific number of decimals.
+        :param number: number
+        :param number_of_decimals: number of decimals
+        :return: truncated number
+        """
+        factor = 10.0 ** number_of_decimals
+        return math.trunc(number * factor) / factor
+
+    for downloaded_image in downloaded_images:
+        annotation_file_name = downloaded_image["file_name"].replace(".jpg", ".txt")
+        print("Converting and writing:", annotation_file_name)
+
+        with open(os.path.join(DATASET_DIRECTORY_NAME, annotation_file_name), "a") as annotation_file:
+            annotations = coco.loadAnns(coco.getAnnIds(imgIds=downloaded_image["id"], catIds=category_id, iscrowd=None))
+            image_width = downloaded_image["width"]
+            image_height = downloaded_image["height"]
+
+            for annotation in annotations:
+                x_min = annotation["bbox"][0]
+                y_min = annotation["bbox"][1]
+                x_max = annotation["bbox"][0] + annotation["bbox"][2]
+                y_max = annotation["bbox"][1] + annotation["bbox"][3]
+
+                x_center = (x_min + x_max) / 2 / image_width
+                y_center = (y_min + y_max) / 2 / image_height
+                width = (x_max - x_min) / image_width
+                height = (y_max - y_min) / image_height
+
+                annotation_file.write(
+                    "0 " + str(truncate(x_center, NUMBER_OF_DECIMALS_TO_LEAVE_IN_ANNOTATIONS)) + " " + str(
+                        truncate(y_center, NUMBER_OF_DECIMALS_TO_LEAVE_IN_ANNOTATIONS)) + " " + str(
+                        truncate(width, NUMBER_OF_DECIMALS_TO_LEAVE_IN_ANNOTATIONS)) + " " + str(
+                        truncate(height, NUMBER_OF_DECIMALS_TO_LEAVE_IN_ANNOTATIONS)) + "\n")
+
+
+def main():
+    """
+    Script entry point.
+    """
+    # Initialize COCO object
+    is_success, coco = initialize_coco()
+    if not is_success:
+        print("Annotations file doesn't exist!")
+        sys.exit(1)
+    print("COCO has been initialized!")
+
+    # Get category ID and images for the particular category name
+    is_success, category_id, images = get_category_id_and_images(coco)
+    if not is_success:
+        print("Such category doesn't exist!")
+        sys.exit(1)
+
+    # Filter images to download
+    is_success, images_to_download = filter_images_to_download(images)
+    if not is_success:
+        print("Unable to filter images to download!")
+        sys.exit(1)
+    print("Images to download have been filtered!")
+
+    # Download and save images
+    download_and_save_images(images_to_download)
+    print("Images have been downloaded and saved!")
+
+    # Convert and write annotations
+    convert_and_write_annotations(coco, images_to_download, category_id)
+    print("Annotations have been converted and written!")
+
+
+if __name__ == "__main__":
+    main()
